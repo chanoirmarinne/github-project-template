e18ea5810e245d419f710ba56c289cda10e62cdc
chanoirmarinne <chanoirmarinne@gmail.com>
2025-04-07 03:10:46 +0000

    Add 28.cpp

---
+++ src/28.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    bool is_student = true;
+    string name = "Alice";
+    cout << "Age: " << age << ", Is Student: " << is_student << ", Name: " << name << endl;
+    return 0;
+}

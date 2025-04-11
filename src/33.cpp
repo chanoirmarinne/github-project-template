2e482479bee3135e04e0bb80e99ee82c5c2a0179
chanoirmarinne <chanoirmarinne@gmail.com>
2025-04-11 12:21:09 +0000

    Add 33.cpp

---
+++ src/33.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 3.14159;
+    char ch = 'A';
+
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    cout << "The value of ch is: " << ch << endl;
+
+    return 0;
+}

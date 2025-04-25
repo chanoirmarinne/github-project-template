5c431a8b0254779e045c13b384bfb990701113ea
chanoirmarinne <chanoirmarinne@gmail.com>
2025-04-25 02:25:58 +0000

    Add 36.cpp

---
+++ src/36.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 5;
+    if (i > 0) {
+        cout << "The value of i is greater than zero." << endl;
+    } else {
+        cout << "The value of i is not greater than zero." << endl;
+    }
+    return 0;
+}
